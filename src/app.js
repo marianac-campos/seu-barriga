39d87c179cbb0dcf83816f90d367f615a219b766
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-04-24 09:38:37 -0300

    feat: creating user route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send();
+});
+
+app.get('/users', (req, res) => {
+  const users = [
+    { name: 'John Doe', email: 'johndoe@email.com' },
+  ];
+
+  res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
