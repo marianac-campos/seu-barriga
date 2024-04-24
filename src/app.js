a9e277ee2372debceb3f8ef4d141c722b793ccc3
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-04-24 09:51:10 -0300

    refactor: creating middleware file

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const app = require('express')();
+const consign = require('consign');
 
-const app = express();
-app.use(bodyParser.json());
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send();
