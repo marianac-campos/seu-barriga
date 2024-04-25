471d4b4dbc7b830e7badf790eb69cd8152d90687
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-04-25 14:45:59 -0300

    feat: creating knexfile

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+  client: 'pg',
+  version: '9.6',
+  connection: {
+    host: 'localhost',
+    user: 'postgres',
+    password: '12345',
+    database: 'barriga',
+  },
+  migrations: {
+    directory: 'src/migrations',
+  },
+};
