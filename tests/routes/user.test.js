23d17ffd3d3be8c4d7af91b7b1f08a8cd4fd4aaa
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-04-26 16:12:42 -0300

    test: creating new test scenarios

--- tests/routes/user.test.js
+++ tests/routes/user.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
+const email = `${Date.now()}@email.com`;
+
 it('should to list all users', () => {
   return request(app).get('/users')
     .then((res) => {
@@ -10,11 +12,46 @@ it('should to list all users', () => {
 });
 
 it('should insert a user with success', () => {
-  const email = `${Date.now()}@email.com`;
   return request(app).post('/users')
-    .send({ name: 'New User', email, password: '1234' })
+    .send({ name: 'John Doe', email, password: '1234' })
     .then((res) => {
       expect(res.status).toBe(201);
-      expect(res.body).toHaveProperty('name', 'New User');
+      expect(res.body).toHaveProperty('name', 'John Doe');
+    });
+});
+
+it('should return an error when name is not defined', () => {
+  return request(app).post('/users')
+    .send({ email, password: '1234' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Name is a mandatory attribute!');
+    });
+});
+
+it('should return an error when email is not defined', () => {
+  return request(app).post('/users')
+    .send({ name: 'John Doe', password: '1234' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Email is a mandatory attribute!');
+    });
+});
+
+it('should return an error when password is not defined', () => {
+  return request(app).post('/users')
+    .send({ name: 'John Doe', email })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Password is a mandatory attribute!');
+    });
+});
+
+it('should insert a user with success', () => {
+  return request(app).post('/users')
+    .send({ name: 'John Doe', email, password: '1234' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('User already created!');
     });
 });
