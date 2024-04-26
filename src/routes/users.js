246e00316f321d732ffa9e2582825f4dad39a01d
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-04-26 15:43:54 -0300

    refactor: including service folder

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,13 @@
 module.exports = (app) => {
-  const findAll = async (req, res) => {
-    const users = await app.db('users').select();
-
-    res.status(200).json(users);
+  const findAll = (req, res) => {
+    app.services.user.findAll()
+      .then((result) => {
+        res.status(200).json(result);
+      });
   };
 
   const create = async (req, res) => {
-    const result = await app.db('users').insert(req.body, '*');
+    const result = await app.services.user.save(req.body);
 
     res.status(201).json(result[0]);
   };
