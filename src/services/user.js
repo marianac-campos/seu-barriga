e36d446ed0062b79c8ad561954ac4bc47a4f196e
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-04-26 16:13:13 -0300

    feat: improving validations in save method

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,18 @@
 module.exports = (app) => {
-  const findAll = () => {
-    return app.db('users').select();
+  const findAll = (filter = {}) => {
+    return app.db('users').where(filter).select();
   };
 
-  const save = (user) => {
+  const save = async (user) => {
+    const { name, email, password } = user;
+    if (!name) return { error: 'Name is a mandatory attribute!' };
+    if (!email) return { error: 'Email is a mandatory attribute!' };
+    if (!password) return { error: 'Password is a mandatory attribute!' };
+
+    const userDb = await findAll({ email });
+
+    if (userDb && userDb.length > 0) return { error: 'User already created!' };
+
     return app.db('users').insert(user, '*');
   };
 
