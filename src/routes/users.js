debf866e30bf1787d5ed82fab149c420a4b0318d
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-05-14 11:42:54 -0300

    feat: adding error manager

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,19 @@
 module.exports = (app) => {
-  const findAll = (req, res) => {
+  const findAll = (req, res, next) => {
     app.services.user.findAll()
       .then((result) => {
         res.status(200).json(result);
-      });
+      })
+      .catch((error) => next(error));
   };
 
-  const create = async (req, res) => {
-    const result = await app.services.user.save(req.body);
-
-    if (result.error) return res.status(400).json(result);
-
-    return res.status(201).json(result[0]);
+  const create = async (req, res, next) => {
+    try {
+      const result = await app.services.user.save(req.body);
+      return res.status(201).json(result[0]);
+    } catch (error) {
+      return next(error);
+    }
   };
 
   return { findAll, create };
