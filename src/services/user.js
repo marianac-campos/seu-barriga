8bc5e89e4db24b288b70685b3954700595e7dff7
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-05-14 11:42:30 -0300

    feat: adding error manager

--- src/services/user.js
+++ src/services/user.js
@@ -1,3 +1,5 @@
+const ValidationError = require('../errors/ValidationError');
+
 module.exports = (app) => {
   const findAll = (filter = {}) => {
     return app.db('users').where(filter).select();
@@ -5,13 +7,14 @@ module.exports = (app) => {
 
   const save = async (user) => {
     const { name, email, password } = user;
-    if (!name) return { error: 'Name is a mandatory attribute!' };
-    if (!email) return { error: 'Email is a mandatory attribute!' };
-    if (!password) return { error: 'Password is a mandatory attribute!' };
+
+    if (!name) throw new ValidationError('Name is a mandatory attribute!');
+    if (!email) throw new ValidationError('Email is a mandatory attribute!');
+    if (!password) throw new ValidationError('Password is a mandatory attribute!');
 
     const userDb = await findAll({ email });
 
-    if (userDb && userDb.length > 0) return { error: 'User already created!' };
+    if (userDb && userDb.length > 0) throw new ValidationError('User already created!');
 
     return app.db('users').insert(user, '*');
   };
