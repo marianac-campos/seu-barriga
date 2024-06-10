29221091b0ca1369c5aa98f444126213a0a4cd82
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-06-10 13:44:52 -0300

    feat: updating logs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
+const winston = require('winston');
+const uuid = require('uuidv4');
 // const knexLogger = require('knex-logger');
 const knexFile = require('../knexfile');
 
-app.db = knex(knexFile.test);
+app.db = knex(knexFile[process.env.NAME]);
 
+app.log = winston.createLogger({
+  leven: 'debug',
+  transports: [
+    new winston.transports.Console({ format: winston.format.json({ space: 1 }) }),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'warn',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json({ space: 1 })),
+    }),
+  ],
+});
 // app.use(knexLogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
@@ -25,7 +38,13 @@ app.use((error, req, res, next) => {
 
   if (name === 'ValidationError') res.status(400).json({ error: message });
   if (name === 'Permission') res.status(403).json({ error: message });
-  res.status(400).json(name, message, stack);
+
+  else {
+    const id = uuid();
+
+    app.log.error({ id, name, message, stack });
+    res.status(400).json({ id, error: 'Falha interna' });
+  }
 
   next(error);
 });
