e6ea74043066a5c00c75e039bd114a88e5cbc490
Mariana Cristina de Campos <CIANDT\marianacampos@lnb029130bhz.ciandt.global>
2024-06-10 13:44:34 -0300

    feat: creating prod env

--- knexfile.js
+++ knexfile.js
@@ -11,4 +11,15 @@ module.exports = {
     migrations: { directory: 'src/migrations' },
     seeds: { directory: 'src/seeds' },
   },
+  prod: {
+    client: 'pg',
+    version: '9.6',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: '12345',
+      database: 'seubarriga',
+    },
+    migrations: { directory: 'src/migrations' },
+  },
 };
